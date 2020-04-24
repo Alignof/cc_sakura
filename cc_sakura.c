9cb136b33e88b0284aac098278570a3fad852dcd
Takana-Norimasa <neuromancer_wg@outlook.jp>
2020-04-24 15:34:44 +0900

    add comment

--- cc_sakura.c
+++ cc_sakura.c
@@ -188,6 +188,38 @@ bool at_eof(){
 	return token->kind==TK_EOF;
 }
 
+void gen(Node *node){
+	if(node->kind==ND_NUM){
+		printf("push %d\n",node->val);
+		return;
+	}
+
+	gen(node->lhs);
+	gen(node->rhs);
+
+	printf("	pop rdi\n");
+	printf("	pop rax\n");
+
+	switch(node->kind){
+		case ND_ADD:
+			printf("	add rax,rdi\n");
+			break;
+		case ND_SUB:
+			printf("	sub rax,rdi\n");
+			break;
+		case ND_MUL:
+			printf("	imul rax,rdi\n");
+			break;
+		case ND_DIV:
+			printf("	cqo\n");
+			printf("	idiv rax,rdi\n");
+			break;
+	}
+
+	printf("	push rax\n");
+
+}
+
 int main(int argc,char **argv){
 	if(argc!=2){
 		fprintf(stderr,"Incorrect number of arguments\n");
@@ -195,28 +227,18 @@ int main(int argc,char **argv){
 	}
 	
 	user_input=argv[1];
+	//tokenize
 	token=tokenize(argv[1]);
+	//make syntax tree
+	Node *node=expr();
 
 	printf(".intel_syntax noprefix\n");
 	printf(".global main\n");
 	printf("main:\n");
 
-	//is_number?
-	printf("	mov rax,%ld\n",expect_number());
+	gen(node);
 
-	while(!at_eof()){
-		//Is the data + ?
-		if(consume('+')){
-			//Is the next data a number?
-			printf("	add rax,%ld\n",expect_number());
-			continue;
-		}
-
-		//Is the data -
-		expect('-');
-		printf("	sub rax,%ld\n",expect_number());
-	}
-
+	printf("	pop rax\n");
 	printf("	ret\n");
 
 	return 0;
