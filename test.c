9e75bbcd1c67c739aa80cc3369d7561c0af98270
Takana-Norimasa <neuromancer_wg@outlook.jp>
2020-11-01 15:20:30 +0900

    [issue] cause segmentation fault when use multi assign and increment at the same time

--- test.c
+++ test.c
@@ -1,7 +1,13 @@
 int main(){
-	int x=4;
-	int y=3;
+	int i;
+	int x[4] = {0,1,2,3};
+	int y[4] = {0,1,2,3};
+	int *p   = x;
+	int *q   = y;
 
-	x+=y;
-	return x;
+	for(i=0;i<4;i++){
+		*(p++) += *(q++);
+	}
+
+	return x[3];
 }
