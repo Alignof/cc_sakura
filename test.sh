4f29e1d6bdb1efc08cf69373f31086ab76b55984
Takana-Norimasa <neuromancer_wg@outlook.jp>
2020-11-05 16:30:50 +0900

    [fix] remove duplication in test.sh

--- test.sh
+++ test.sh
@@ -167,12 +167,6 @@ assert -cl 1 "int main(){int flag; char *hello=\"hello\"; if(hello[1] == 'e') fl
 
 assert -cl 5 "struct test{int a; int b;}; int main(){struct test x; x.a=2; x.b=3; return x.a + x.b;}"
 assert -cl 5 "struct test{char a; int b;}; int main(){struct test x; x.a=2; x.b=3; return x.a + x.b;}"
-
-assert -cl 97 "int main(){return 'a';}"
-assert -cl 1 "int main(){int flag; char *hello=\"hello\"; if(hello[1] == 'e') flag=1; else flag=0; return flag;}"
-
-assert -cl 5 "struct test{int a; int b;}; int main(){struct test x; x.a=2; x.b=3; return x.a + x.b;}"
-assert -cl 5 "struct test{char a; int b;}; int main(){struct test x; x.a=2; x.b=3; return x.a + x.b;}"
 assert -cl 5 "struct test{int a; int b;}; int main(){struct test x; struct test *y; y=&x; y->a=2; y->b=3; return y->a + y->b;}"
 assert -cl 5 "struct test{int a; int b;}; int main(){struct test x; struct test *y; struct test **z; y=&x; z=&y; (*z)->a=2; (*z)->b=3; return (*z)->a + (*z)->b;}"
 
