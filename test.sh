6b3c8bf95b960557fe2aa47f8abad3b49e38d21e
Takana-Norimasa <neuromancer_wg@outlook.jp>
2020-11-13 01:59:01 +0900

    [issue] error occurred when test 3d array

--- test.sh
+++ test.sh
@@ -151,6 +151,8 @@ assert -cl 72 'int main(){int i; int k; int x[10][10]; for(i=1;i<=9;i++){ for(k=
 assert -cl 72 'int x[10][10]; int main(){int i; int k; for(i=1;i<=9;i++){ for(k=1;k<=9;k++){ x[k][i]=i*k; }} return x[8][9];}'
 assert -cl 5 "int main(){int x[2][3]; int *y; x[1][2]=5; y=x[1]; y[2];}"
 assert -cl 8 "int main(){int x[2][3]; int *y; int *z; x[1][1]=5; x[1][2]=3; y=x[1]; z=y; return *(y+1) + z[2];}"
+assert -cl 8 "int main(){int x[2][3][4]; x[1][1][1]=5; x[1][2][3]=3; return x[1][1][1] + x[1][2][3]}"
+assert -cl 8 "int main(){int x[2][3][4]; int **y; int **z; x[1][1][1]=5; x[1][2][3]=3; y=x[1]; z=y; return *(y+1)[1] + (z[2]+3);}"
 
 assert -cl 5  "int a=8; int main(){a=a-3;a;}"
 assert -cl 108 'char *x="hello"; int main(){*(x+2);}'
