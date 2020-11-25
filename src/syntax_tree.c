9f16646db1b918e3c80d39049a5f998edbcbc556
Takana-Norimasa <neuromancer_wg@outlook.jp>
2020-11-25 20:29:31 +0900

    [wip][update] remove label case and default from the chain list

--- src/syntax_tree.c
+++ src/syntax_tree.c
@@ -394,7 +394,7 @@ Node *stmt(void){
  
  	}else if(consume_reserved_word("switch", TK_SWITCH)){
  		/*
- 		 * default<---switch----+---------->block code
+ 		 * default<---switch--->block code
  		 *               | 
  		 *               | (next)
  		 *               | 
@@ -404,7 +404,10 @@ Node *stmt(void){
  		 *               +----->case->case->... 
  		 */
  
- 		Node *cond = NULL;
+ 		Node  *cond	 = NULL;
+		Label *tmp_label = labels;
+		Label *prev;
+
  		node = new_node(ND_SWITCH, node, NULL);
  		if(consume("(")){
  			//jmp expr
@@ -415,8 +418,39 @@ Node *stmt(void){
  			error_at(token->str, "expected ‘(’ before ‘{’ token");
  		}
 
+		// get code block 
 		node->rhs = stmt(); 
- 
+
+		// register and remove case
+		Node *cond_cases;
+		prev = tmp_label;
+		Label *lb = tmp_label->next;
+		while(lb){
+			if(lb->kind != LB_LABEL){
+				if(lb->kind == LB_CASE){
+					if(cond_cases){
+						cond_cases->next = new_node(ND_EQ, cond, lb->cond);
+						cond_cases       = cond_cases->vector;
+					}else{
+						cond_cases = new_node(ND_EQ, cond, lb->cond);
+						node->next = cond_cases;
+					}
+				}else if(lb->kind == LB_DEFAULT){
+					if(node->lhs){
+						error_at(token->str, "multiple default labels in one switch");
+					}else{
+						node->lhs = new_node(ND_EQ, cond, lb->cond);
+					}
+				}
+
+				prev->next = lb->next;
+				free(lb);
+				lb = prev->next;
+			}else{
+				prev = lb;
+				lb = lb->next;
+			}
+		}
 	}else if(consume_reserved_word("case", TK_CASE)){
 		/*
 		 *  (cond) <--- case ---> code
@@ -433,82 +467,6 @@ Node *stmt(void){
 		expect(":");
 		node->rhs = stmt();
 		label_register(node, LB_DEFAULT);
-// 
-// 	}else if(consume_reserved_word("switch", TK_SWITCH)){
-// 		/*
-// 		 * default<--switch----+
-// 		 *                     | (rhs)
-// 		 *                     | 
-// 		 *         (cond)<---case--->in_label(codes)
-// 		 *                     |          | 
-// 		 *                     |          | (vector: in_label)
-// 		 *                     |          +----->code->code->... 
-// 		 *                     | 
-// 		 *                     | (next: chain_case)
-// 		 *                     +----->case->case->... 
-// 		 */
-// 
-// 		Node *cond = NULL;
-// 		node = new_node(ND_SWITCH, node, NULL);
-// 		if(consume("(")){
-// 			//jmp expr
-// 			cond = expr();
-// 			//check end of caret
-// 			expect(")");
-// 		}else{
-// 			error_at(token->str, "expected ‘(’ before ‘{’ token");
-// 		}
-// 
-// 		Node *chain_case = NULL;
-// 		expect("{");
-// 		while(token->kind == TK_CASE || token->kind == TK_DEFAULT){
-// 			if(consume_reserved_word("case", TK_CASE)){
-// 				if(chain_case){
-// 					chain_case->vector = new_node(ND_CASE, new_node(ND_EQ, cond, logical()), NULL);
-// 					chain_case         = chain_case->vector;
-// 				}else{
-// 					chain_case   = new_node(ND_CASE, new_node(ND_EQ, cond, logical()), NULL);
-// 					node->rhs    = chain_case;
-// 				}
-// 				expect(":");
-// 
-// 				Node *in_label = NULL;
-// 				while(token->kind != TK_CASE && token->kind != TK_DEFAULT){
-// 					if(in_label){
-// 						in_label->vector = stmt();
-// 						in_label = in_label->vector;
-// 					}else{
-// 						in_label = stmt();
-// 						chain_case->rhs = in_label;
-// 					}
-// 
-// 					if(check("}")) break;
-// 				}
-// 			}else if(consume_reserved_word("default", TK_DEFAULT)){
-// 				expect(":");
-// 				if(node->lhs == NULL){
-// 					Node *in_label = NULL;
-// 					node->lhs = new_node(ND_CASE, NULL, NULL);
-// 					while(token->kind != TK_CASE && token->kind != TK_DEFAULT){
-// 						if(in_label){
-// 							in_label->vector = stmt();
-// 							in_label = in_label->vector;
-// 						}else{
-// 							in_label       = new_node(ND_CASE, NULL, stmt());
-// 							node->lhs->rhs = in_label;
-// 						}
-// 
-// 						if(check("}")) break;
-// 					}
-// 				}else{
-// 					error_at(token->str, "multiple default labels in one switch");
-// 				}
-// 			}else{
-// 				error_at(token->str, "statement will never be executed");
-// 			}
-// 		}
-// 		expect("}");
-//
 	}else if(consume_reserved_word("for", TK_FOR)){
 		node = new_node(ND_FOR, node, NULL);
 		if(consume("(")){
