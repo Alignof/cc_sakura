ec422da975b0751e0e0aec1cbbf6a79a16b8b261
Takana-Norimasa <neuromancer_wg@outlook.jp>
2020-11-25 18:19:00 +0900

    [issue] it does not work when compile Duff's device

--- test.c
+++ test.c
@@ -1,8 +1,22 @@
 int main(){
-	int i=0;
-	do{
-		i++;
-	}while(i == 5);
+	char *psz1 = calloc(100, sizeof(char));
+	char *psz2 = "abcdefghijklmnopqrstuvwxyz";
+	char *to   = psz1;
+	char *from = psz2;
+	int  count = 26;
 
-	return i;
+	switch (count % 8) {
+		case 0:  do {  *to++ = *from++;
+		case 7:        *to++ = *from++;
+		case 6:        *to++ = *from++;
+		case 5:        *to++ = *from++;
+		case 4:        *to++ = *from++;
+		case 3:        *to++ = *from++;
+		case 2:        *to++ = *from++;
+		case 1:        *to++ = *from++;
+			} while ((count -= 8) > 0);
+	}
+
+	printf("to	:%s\n", psz1);
+	printf("from	:%s\n", psz2);
 }
