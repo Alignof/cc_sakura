0f7e5e36341e4193571f76e8b9fc336c8d401a61
Takana-Norimasa <neuromancer_wg@outlook.jp>
2020-11-28 00:40:07 +0900

    [edit] edit README.md

--- test.c
+++ test.c
@@ -1,16 +1,14 @@
 int main(){
 	int x=0;
+	int y=1;
 	int z=2;
 	if(x == 0){
 		if(z==2){
 			int x=2;
 			int y=4;
 			z=x+y;
-			printf("[if] z:%d\n", z);
 		}
 	}
-	printf("[func] z:%d\n", z);
-	int y=1;
 	return y+z;
 }
 
