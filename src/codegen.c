92806da54fc5114155a98d9c91230a0ec96f6bc6
Takana-Norimasa <neuromancer_wg@outlook.jp>
2020-12-01 16:30:33 +0900

    [issue] need symbol extern

--- src/codegen.c
+++ src/codegen.c
@@ -26,6 +26,7 @@ void expand_block_code(Node *node){
 
 void gen_gvar(Node *node){
 	printf("	lea rax,  %.*s[rip]\n", node->val, node->str);
+	//printf("	lea rax,  _%.*s[rip]\n", node->val, node->str);
 	printf("	push rax\n");
 }
 
