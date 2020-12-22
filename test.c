1fdc6e6ca183e23d912191def06051f6c3898f4b
Takana-Norimasa <neuromancer_wg@outlook.jp>
2020-12-22 21:41:14 +0900

    [issue] Wrong register to compare conditions

--- test.c
+++ test.c
@@ -1,8 +1,44 @@
-int main(){
-	char arr[] = "+123456";
-
-	printf("arr:%s\n", arr);
-	printf("size of arr:%d\n", sizeof(arr));
-
-	return 0;
-}
+// typedef struct Test Test;
+// struct Test{
+// 	int value;
+// 	Test *next;
+// };
+// 
+// void prepare(Test *test){
+// 	int i;
+// 	test->value = 0;
+// 	for(int i=1;i<8;i++){
+// 		test->value  = i;
+// 		test->next = calloc(1, sizeof(Test));
+// 		test = test->next;
+// 	}
+// }
+// 
+// void expand_next(Test *test){
+// 	printf("test0: %d\n", test->value);
+// 	printf("test1: %d\n", test->next->value);
+// 	printf("test2: %d\n", test->next->next->value);
+// 
+// 	while(test){
+// 		printf("%d\n", test->value);
+// 		test=test->next;
+// 	}
+// 	printf("	push rax\n");
+// }
+// 
+// int main(){
+// 	Test *test = calloc(1, sizeof(Test));
+// 	prepare(test);
+// 
+// 	if(test){
+// 		printf("%d\n", test->value);
+// 	}else{
+// 		printf("NULL\n");
+// 	}
+// 
+// 	//expand_next(test);
+// 
+// 	return 0;
+// }
+// 
+char x[6]="hello"; int main(){*(x+2);}
